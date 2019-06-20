3dace6be4bfe4a1999c9fc57516084fd4795768f
juliano lab pc <julianoferrarigianlupi@gmail.com>
2019-06-20 11:24:00 -0400

    changed paths for my lab machine

--- windows/build_scripts_py3/build_all.bat
+++ windows/build_scripts_py3/build_all.bat
@@ -1,12 +1,12 @@
 
 @set CURRENT_DIRECTORY=%CD%
-@set PYTHONPATH=D:\CC3D_BUILD_SCRIPTS_GIT
+@set PYTHONPATH=C:\cc3d_dev\cc3d_build_scripts
 
 @set version=4.0.0
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-c:\Miniconda3\python win_cc3d_builder.py  -p D:/install_projects/%version%-64bit -s D:/CC3D_PY3_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json
+c:\Miniconda3\python win_cc3d_builder.py  -p C:/install_projects/%version%-64bit -s C:/cc3d_dev/py3/cc3d_git  -i C:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json
 
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
