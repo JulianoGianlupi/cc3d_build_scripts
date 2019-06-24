8a3f12121c94c42a78c8b4eed1f0e02fde0e4aeb
juliano lab pc <julianoferrarigianlupi@gmail.com>
2019-06-24 12:07:06 -0400

    Update build_all.bat

--- windows/build_scripts_py3/build_all.bat
+++ windows/build_scripts_py3/build_all.bat
@@ -12,7 +12,7 @@ rem C:\Users\jferrari\AppData\Local\Continuum\anaconda3\envs\conda_2020\python w
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-C:\Users\jferrari\AppData\Local\Continuum\anaconda3\envs\conda_2020\python .\win_cc3d_builder.py  -p C:/install_projects/%version%-64bit -s C:/CC3D_PY3_GIT  -i C:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json --gpu
+C:\Users\jferrari\AppData\Local\Continuum\anaconda3\envs\conda_2020\python .\win_cc3d_builder.py  -p C:/install_projects/%version%-64bit -s C:/CC3D_PY3_GIT  -i C:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json 
 
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
